717f20619a371ef632e32e580dc23367f6d562a8
SynthCyraX <SynthCyraX@gmail.com>
2023-12-02 11:12:19 +0100

    First draft styling Sidebar.jsx

--- frontend/src/component/Home.jsx
+++ frontend/src/component/Home.jsx
@@ -1,4 +1,4 @@
-import { Link, Outlet } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 import styles from './Home.module.css';
 import SideBar from './SideBar';
 
--- frontend/src/component/SideBar.jsx
+++ frontend/src/component/SideBar.jsx
@@ -4,41 +4,55 @@ import styles from './SideBar.module.css';
 export default function SideBar() {
   return (
     <>
-      <div className={styles.userIcon}></div>
-      <Link to={'/home/viewprofile'}>ViewProfile</Link>
+      <div className={styles.sidebar}>
+        <div className={styles.userIcon}>
+          <Link to={'/home/viewprofile'}>ViewProfile</Link>
+        </div>
+        <div className={styles.searchSection}>
+          <input type="text" placeholder="Search User" />
+          <button>Search</button>
+        </div>
 
-      <input type="text" placeholder="Search User" />
-      <button>Search</button>
-      <h1>People</h1>
-      <div className="peopleContainer">
-        <div className="person">User1</div>
-        <div className="person">User2</div>
+        <h1>People</h1>
+
+        <div className={styles.personContainer}>
+          <div className={styles.person}>User1</div>
+          <div className={styles.person}>User2</div>
+          <div className={styles.person}>User2</div>
+          <div className={styles.person}>User2</div>
+          <div className={styles.person}>User2</div>
+          <div className={styles.person}>User2</div>
+          <div className={styles.person}>User2</div>
+          <Link to={'/home/privatechat'}>PrivateChat</Link>
+        </div>
+
+        <h1>Chatroom</h1>
+
+        <div className={styles.chatroomContainer}>
+          <div className={styles.chatroom}>Chat1</div>
+          <div className={styles.chatroom}>Chat2</div>
+          <div className={styles.chatroom}>Chat2</div>
+          <div className={styles.chatroom}>Chat2</div>
+          <div className={styles.chatroom}>Chat2</div>
+          <div className={styles.chatroom}>Chat2</div>
+          <div className={styles.chatroom}>Chat2</div>
+          <Link to={'/home/chatroom'}>ChatRoom</Link>
+        </div>
+
+        <div className={styles.editLogoutContainer}>
+          <button>
+            Edit Profile
+            <Link to={'/home/editprofile'}>EditProfile</Link>
+          </button>
+
+          <button>
+            Logout
+            <Link to={'/logout'}>Logout</Link>
+          </button>
+        </div>
+
+        {/* <Link to={'/home'}>Home</Link> */}
       </div>
-      <Link to={'/home/privatechat'}>PrivateChat</Link>
-
-      <h1>Chatroom</h1>
-      <div className="chatroomContainer">
-        <div className="chatroom">Chat1</div>
-        <div className="chatroom">Chat2</div>
-      </div>
-      <Link to={'/home/chatroom'}>ChatRoom</Link>
-
-      <div className="editLogoutContainer">
-        <button>Edit Profile</button>
-        <Link to={'/home/editprofile'}>EditProfile</Link>
-
-        <button>Logout</button>
-        <Link to={'/logout'}>Logout</Link>
-      </div>
-
-      <ul>
-        <Link to={'/home'}>Home</Link>
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-      </ul>
     </>
   );
 }
