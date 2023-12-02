277c51150de5f45f0e5615d3c391d9775424ffd1
SynthCyraX <SynthCyraX@gmail.com>
2023-12-02 09:21:07 +0100

    Added SignUp.jsx and styling

--- frontend/src/component/SignUp.jsx
+++ frontend/src/component/SignUp.jsx
@@ -1,10 +1,39 @@
 import { Link } from 'react-router-dom';
-
+import styles from './Login.module.css';
+import logo from '/logo.svg';
 export default function SignUp() {
   return (
     <>
-      <p>SignUp</p>
-      <Link to={'/'}>Login</Link>
+      <main className={styles.main}>
+        <div className={styles.companyLogo}>
+          <img src={logo} alt="" />
+          <h1>Chatty</h1>
+        </div>
+        <form className={styles.loginForm}>
+          <input
+            type="text"
+            name="username"
+            id="username"
+            placeholder="Choose Username"
+          />
+          <input
+            type="password"
+            name="password"
+            id="password"
+            placeholder="Choose Password"
+          />
+          <input
+            type="password"
+            name="password"
+            id="password"
+            placeholder="Repeat Password"
+          />
+          <button type="submit">Sign Up</button>
+          <Link to={'/'}>Login</Link>
+        </form>
+        {/* <p>Login</p>
+        <Link to={'/home'}>Home</Link> */}
+      </main>
     </>
   );
 }
