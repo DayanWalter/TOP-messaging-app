f8f04a1e727a554b71a0897e3ea05215c640bfc9
SynthCyraX <SynthCyraX@gmail.com>
2023-12-03 09:46:28 +0100

    EditProfile and ViewProfile first draft done

--- frontend/src/component/EditProfile.jsx
+++ frontend/src/component/EditProfile.jsx
@@ -1,10 +1,86 @@
 import { Link } from 'react-router-dom';
+import styles from './EditProfile.module.css';
+import { useState } from 'react';
+
+// Offline
 
 export default function EditProfile() {
+  const [profile, setProfile] = useState({
+    description: 'I love reading books',
+    name: 'Jonathan Frakes',
+    username: 'JFrakes',
+  });
+
+  const handleChange = (key, e) => {
+    const newProfile = {
+      ...profile,
+      [key]: e.currentTarget.value,
+    };
+    setProfile(newProfile);
+  };
+
   return (
     <>
-      <p>EditProfile</p>
-      <Link to={'/home'}>Home</Link>
+      <div className={styles.site}>
+        <div className={styles.content}>
+          <header className={styles.header}>
+            <Link to={'/home/viewprofile'}>
+              <div className={styles.userIcon}></div>
+            </Link>
+          </header>
+          <main className={styles.main}>
+            <div className={styles.label}>
+              <div className={styles.labelName}>Description</div>
+              <div className={styles.labelContent}>
+                <input
+                  type="text"
+                  name=""
+                  id=""
+                  value={profile.description}
+                  onChange={(e) => {
+                    handleChange('description', e);
+                  }}
+                />
+              </div>
+            </div>
+            <div className={styles.label}>
+              <div className={styles.labelName}>Name</div>
+              <div className={styles.labelContent}>
+                <input
+                  type="text"
+                  name=""
+                  id=""
+                  value={profile.name}
+                  onChange={(e) => {
+                    handleChange('name', e);
+                  }}
+                />
+              </div>
+            </div>
+            <div className={styles.label}>
+              <div className={styles.labelName}>Username</div>
+              <div className={styles.labelContent}>
+                <input
+                  type="text"
+                  name=""
+                  id=""
+                  value={profile.username}
+                  onChange={(e) => {
+                    handleChange('username', e);
+                  }}
+                />
+              </div>
+            </div>
+            <div className={styles.button}>
+              <Link to={'/home'}>
+                {/* Send changed profile to server */}
+                <button>Save changes</button>
+              </Link>
+            </div>
+          </main>
+          <footer className={styles.footer}></footer>
+        </div>
+      </div>
     </>
   );
 }
--- frontend/src/component/ViewProfile.jsx
+++ frontend/src/component/ViewProfile.jsx
@@ -1,10 +1,47 @@
 import { Link } from 'react-router-dom';
+import styles from './ViewProfile.module.css';
 
+// Offline
+const profile = {
+  description: 'I love reading books',
+  name: 'Jonathan Frakes',
+  username: 'JFrakes',
+};
 export default function ViewProfile() {
   return (
     <>
-      <p>ViewProfile</p>
-      <Link to={'/home'}>Home</Link>
+      <div className={styles.site}>
+        <div className={styles.content}>
+          <header className={styles.header}>
+            <Link to={'/home/viewprofile'}>
+              <div className={styles.userIcon}></div>
+            </Link>
+          </header>
+          <main className={styles.main}>
+            <div className={styles.label}>
+              <div className={styles.labelName}>Description</div>
+              <div className={styles.labelContent}>{profile.description}</div>
+            </div>
+            <div className={styles.label}>
+              <div className={styles.labelName}>Name</div>
+              <div className={styles.labelContent}>{profile.name}</div>
+            </div>
+            <div className={styles.label}>
+              <div className={styles.labelName}>Username</div>
+              <div className={styles.labelContent}>{profile.username}</div>
+            </div>
+            <div className={styles.button}>
+              <Link to={'/home'}>
+                <button>Add to friends</button>
+              </Link>
+              <Link to={'/home/privatechat'}>
+                <button>Start to chat</button>
+              </Link>
+            </div>
+          </main>
+          <footer className={styles.footer}></footer>
+        </div>
+      </div>
     </>
   );
 }
