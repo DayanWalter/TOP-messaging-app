9acc3189b96263ba1fb07adb569bf2a74ae8d11a
SynthCyraX <SynthCyraX@gmail.com>
2023-12-14 08:29:27 +0100

    Added comments to backend

--- backend/app.js
+++ backend/app.js
@@ -1,36 +1,47 @@
+// What is createError for?
 const createError = require('http-errors');
+// define express(more precise!)
 const express = require('express');
 // What is cors for?(Multiple req...)
 const cors = require('cors');
-
+// What is path for?
 const path = require('path');
+// What is cookie-parser for?
 const cookieParser = require('cookie-parser');
+// What is logger for?
 const logger = require('morgan');
 
+// Define routes
 const indexRouter = require('./routes/index');
 
 // Hide sensible data
 require('dotenv').config();
 
+// Define whole app
 const app = express();
 
+// Set database up
 const mongoose = require('mongoose');
+// What is this for?
 mongoose.set('strictQuery', false);
 
+// Take local defined db or take db from variable via PaaS
 const mongoDB = process.env.MONGODB_URI || process.env.DEV_DB_URL;
 
+// Connect to db and log err if err is caught
 main().catch((err) => console.log(err));
 async function main() {
   await mongoose.connect(mongoDB);
 }
+// Again:?
 app.use(cors());
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// use / as starting point for router
 app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
