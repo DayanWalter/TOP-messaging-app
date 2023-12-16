57da6aeea224e7b74ea8a84225fb7c2b3fbde8c6
SynthCyraX <SynthCyraX@gmail.com>
2023-12-16 03:36:46 +0100

    Renamed friend to receiver

--- frontend/src/component/PrivateChat.jsx
+++ frontend/src/component/PrivateChat.jsx
@@ -29,42 +29,44 @@ const exampleMessages = [
 export default function ChatRoom() {
   // Get params for receiver
   const loaderData = useLoaderData();
+  const receiverId = loaderData.id;
   // console.log(loaderData.id);
 
   const token = localStorage.getItem('jwtoken');
   // Split the payload of the jwt and convert the username-part
   const payload = JSON.parse(atob(token.split('.')[1]));
   // Define the Username you are looking for
-  const searchedUsername = payload.username;
+  const senderUsername = payload.username;
 
-  const [friend, setFriend] = useState();
+  const [receiver, setReceiver] = useState();
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(true);
-  const [text, setText] = useState('');
   const [messages, setMessages] = useState(exampleMessages);
 
   // Save input from form
   const [formData, setFormData] = useState({
-    username: '',
+    sender: '',
+    receiver: '',
     text: '',
   });
 
-  // hardcore the _id first
-  const sender = { _id: 'Dave._id' };
   // at every keystroke, change the formdata
   const handleChange = (e) => {
+    const { name, value } = e.target;
+
     const newMessage = {
       ...formData,
-      text: e.currentTarget.value,
-      // _id from the sender
-      username: sender._id,
+      [name]: value,
+      receiver: receiverId,
+      sender: senderUsername,
     };
     setFormData(newMessage);
   };
   console.log(formData);
+  
   // create chatroom
   useEffect(() => {
-    const getDetailsFromFriend = async () => {
+    const getDetailsFromReceiver = async () => {
       try {
         const response = await fetch(
           `http://localhost:3000/api/user/${loaderData.id}`
@@ -75,30 +77,51 @@ export default function ChatRoom() {
 
         const data = await response.json();
 
-        setFriend(data.user);
+        setReceiver(data.user);
         setError(null);
       } catch (error) {
         setError(error.message);
-        setFriend(null);
+        setReceiver(null);
       } finally {
         setLoading(false);
       }
     };
-    getDetailsFromFriend();
+    getDetailsFromReceiver();
   }, [loaderData]);
-  // console.log(friend);
+  // console.log(Receiver);
 
   // Offline
-  const formSubmit = (e) => {
+  const formSubmit = async (e) => {
     e.preventDefault();
-    // send text, sender and receiver to backend
+    // POST text, sender and receiver to backend
+    try {
+      const response = await fetch(
+        `http://localhost:3000/api/message/${receiverId}/create`,
+        {
+          method: 'POST',
+          body: JSON.stringify(formData),
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        }
+      );
+
+      if (response.ok) {
+        const json = await response.json();
+        console.log(json);
+      } else {
+        console.error('Failed to login');
+      }
+    } catch (error) {
+      console.error('Error:', error);
+    }
   };
   return (
     <>
       <div className={styles.site}>
         <div className={styles.content}>
           <header className={styles.header}>
-            {friend && <h1>{friend.username}</h1>}
+            {receiver && <h1>{receiver.username}</h1>}
           </header>
           <main className={styles.main}>
             {/* Map over the messages */}
@@ -106,7 +129,7 @@ export default function ChatRoom() {
               {messages.map(({ _id, sender, text, time }) => (
                 <li key={_id}>
                   <Link to={`/home/viewprofile/${_id}`}>{sender}</Link>
-                  {friend && <p>{friend.messages}</p>}
+                  {receiver && <p>{receiver.messages}</p>}
 
                   <Message text={text} time={time} />
                 </li>
@@ -116,6 +139,7 @@ export default function ChatRoom() {
           <footer className={styles.footer}>
             <form onSubmit={formSubmit}>
               <input
+                name="text"
                 type="text"
                 placeholder="Enter message..."
                 value={formData.text}
