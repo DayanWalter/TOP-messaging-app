1f93b25ab2c0ab854454b2af03d59fae23c3a976
SynthCyraX <SynthCyraX@gmail.com>
2023-12-22 06:20:43 +0100

    MessageController refactored

--- backend/controllers/messageController.js
+++ backend/controllers/messageController.js
@@ -1,18 +1,19 @@
 const Message = require('../models/message');
-const User = require('../models/user');
 const asyncHandler = require('express-async-handler');
-
-// GET messages from user
+/// MESSAGECONTROLLER DONE ///
+// GET messages from user(done)
 exports.message_user_get = asyncHandler(async (req, res, next) => {
-  const receiverId = req.params.receiver;
+  // _id is from jwt.js(user object in done function)
   const userId = req.user._id;
+  // The receiver is in the params
+  const receiverId = req.params.receiver;
 
   // Find all messages in which the...
   const messages = await Message.find({
     $or: [
       // receiver is in the params, OR...
       { 'receiver.user': receiverId, sender: userId },
-      // receiver is in the token
+      // receiver is in the jwtoken
       { 'receiver.user': userId, sender: receiverId },
     ],
   })
@@ -22,84 +23,61 @@ exports.message_user_get = asyncHandler(async (req, res, next) => {
 
   res.json({ messages });
 });
-// POST message to user
+// POST message to user(done)
 exports.message_user_post = asyncHandler(async (req, res, next) => {
   // _id is from jwt.js(user object in done function)
-  const senderId = req.user._id;
+  const userId = req.user._id;
+
   // The receiver is in the params
   const receiverId = req.params.receiver;
 
   const userMessage = new Message({
-    sender: senderId,
+    sender: userId,
     receiver: {
+      // The receiver is a user
       user: receiverId,
     },
     text: req.body.text,
   });
 
-  // save message in backend
+  // save message in db
   const savedMessage = await userMessage.save();
-
-  // add message._id to sender
-  await User.findByIdAndUpdate(
-    senderId,
-    { $push: { messages: savedMessage._id } },
-    { new: true }
-  );
-
-  // add message._id to receiver
-  await User.findByIdAndUpdate(
-    receiverId,
-    { $push: { messages: savedMessage._id } },
-    { new: true }
-  );
-
+  // send saved message to frontend
   res.json({ userMessage: savedMessage });
 });
 
-// GET messages from group
+// GET messages from group(done)
 exports.message_group_get = asyncHandler(async (req, res, next) => {
   const receiverId = req.params.receiver;
-
   // Find all messages in which the...
   const messages = await Message.find({
+    // receiver(group) is in the params
     'receiver.group': receiverId,
   })
+    // Projection for just the "username"
     .populate('sender', 'username')
     .exec();
   res.json({ messages });
 });
-// POST message to group
+// POST message to group(done)
 exports.message_group_post = asyncHandler(async (req, res, next) => {
   // _id is from jwt.js(user object in done function)
-  const senderId = req.user._id;
+  const userId = req.user._id;
+
   // The receiver is in the params
   const receiverId = req.params.receiver;
 
   const groupMessage = new Message({
-    sender: senderId,
+    sender: userId,
     receiver: {
+      // The receiver is a group
       group: receiverId,
     },
     text: req.body.text,
   });
 
-  // save message in backend
+  // save message in db
   const savedMessage = await groupMessage.save();
-
-  // // add message._id to sender
-  // await User.findByIdAndUpdate(
-  //   senderId,
-  //   { $push: { messages: savedMessage._id } },
-  //   { new: true }
-  // );
-
-  // // add message._id to receiver
-  // await Group.findByIdAndUpdate(
-  //   receiverId,
-  //   { $push: { messages: savedMessage._id } },
-  //   { new: true }
-  // );
-
+  // send saved message to frontend
   res.json({ groupMessage: savedMessage });
 });
